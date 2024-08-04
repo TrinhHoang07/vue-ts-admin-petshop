46c7bed52ff26175e49d393824cb45d9fe91053e
Trịnh Văn Hoàng <hoang0702bmw@gmail.com>
2024-08-04 23:12:59 +0700

    update code

--- src/components/ModalAddCustomer.vue
+++ src/components/ModalAddCustomer.vue
@@ -2,7 +2,7 @@
 import { ApiService } from '@/axios/ApiService';
 import { Form, Input, InputNumber, Modal } from 'ant-design-vue';
 import { useToast } from 'primevue/usetoast';
-import { ref, watch } from 'vue';
+import { ref, watch, reactive } from 'vue';
 
 type T_Props = {
     open: any;
@@ -13,6 +13,33 @@ type T_Props = {
 const props = defineProps<T_Props>();
 const apiService = new ApiService();
 const toast = useToast();
+
+const errors = reactive<
+    | {
+          name: boolean;
+          email: boolean;
+          password: boolean;
+          address: boolean;
+          gender: boolean;
+          phone_number: boolean;
+          avatar_path: boolean;
+          birth_date: boolean;
+      }
+    | any
+>({
+    name: false,
+    email: false,
+    password: false,
+    address: false,
+    gender: false,
+    phone_number: false,
+    avatar_path: false,
+    birth_date: false,
+});
+
+const REGEXPASS = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+const REGEX_EMAIL = /^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/;
+const REGEX_PHONE = /(|0[3|5|7|8|9])+([0-9]{8})\b/g;
 
 const name = ref<string>('');
 const password = ref<string>('');
@@ -34,49 +61,100 @@ watch(props.open, () => {
     birth_date.value = '';
 });
 
-const handleFinish = () => {
-    const values = {
-        name: name.value,
-        password: password.value,
-        email: email.value,
-        avatar_path: avatar_path.value,
-        address: address.value,
-        gender: gender.value,
-        birth_date: birth_date.value,
-        phone_number: phone_number.value,
-    };
+const handleError = (name: string) => {
+    errors[`${name}`] = true;
+};
 
-    apiService.customers
-        .createCustomer(values)
-        .then((res) => {
-            console.log(res);
-            if (res.message === 'success') {
+const handleClearError = (name: string) => {
+    errors[`${name}`] = false;
+};
+
+const handleFinish = () => {
+    if (!name.value.trim()) {
+        handleError('name');
+    } else {
+        handleClearError('name');
+    }
+    if (!REGEX_EMAIL.test(email.value.trim())) {
+        handleError('email');
+    } else {
+        handleClearError('email');
+    }
+    if (!REGEXPASS.test(password.value.trim())) {
+        handleError('password');
+    } else {
+        handleClearError('password');
+    }
+    if (!address.value.trim()) {
+        handleError('address');
+    } else {
+        handleClearError('address');
+    }
+    if (!REGEX_PHONE.test(phone_number.value.trim())) {
+        handleError('phone_number');
+    } else {
+        handleClearError('phone_number');
+    }
+    if (!avatar_path.value.trim()) {
+        handleError('avatar_path');
+    } else {
+        handleClearError('avatar_path');
+    }
+    if (!birth_date.value.trim()) {
+        handleError('birth_date');
+    } else {
+        handleClearError('birth_date');
+    }
+    if (!gender.value.trim()) {
+        handleError('gender');
+    } else {
+        handleClearError('gender');
+    }
+
+    if ((Object as any).entries(errors).every((item: any) => item[1] === false)) {
+        const values = {
+            name: name.value,
+            password: password.value,
+            email: email.value,
+            avatar_path: avatar_path.value,
+            address: address.value,
+            gender: gender.value,
+            birth_date: birth_date.value,
+            phone_number: phone_number.value,
+        };
+
+        apiService.customers
+            .createCustomer(values)
+            .then((res) => {
+                console.log(res);
+                if (res.message === 'success') {
+                    toast.add({
+                        severity: 'success',
+                        detail: 'Product created successfully',
+                        life: 3000,
+                    });
+                    props.setDataSource({
+                        key: res.data.id,
+                        address: res.data.address,
+                        avatarPath: res.data.avatar_path,
+                        birthDate: res.data.birth_date,
+                        email: res.data.email,
+                        gender: res.data.gender,
+                        name: res.data.name,
+                        phoneNumber: res.data.phone_number,
+                        id: res.data.id,
+                    });
+                    props.setOpen(false);
+                }
+            })
+            .catch((_) => {
                 toast.add({
-                    severity: 'success',
-                    detail: 'Product created successfully',
+                    severity: 'error',
+                    detail: 'ERROR !!!',
                     life: 3000,
                 });
-                props.setDataSource({
-                    key: res.data.id,
-                    address: res.data.address,
-                    avatarPath: res.data.avatar_path,
-                    birthDate: res.data.birth_date,
-                    email: res.data.email,
-                    gender: res.data.gender,
-                    name: res.data.name,
-                    phoneNumber: res.data.phone_number,
-                    id: res.data.id,
-                });
-                props.setOpen(false);
-            }
-        })
-        .catch((_) => {
-            toast.add({
-                severity: 'error',
-                detail: 'ERROR !!!',
-                life: 3000,
             });
-        });
+    }
 };
 </script>
 
@@ -93,30 +171,103 @@ const handleFinish = () => {
         <div class="form-container">
             <Form layout="vertical" autoComplete="off" name="add_customer">
                 <Form.Item name="name" label="Tên người dùng">
-                    <Input v-model:value="name" />
+                    <Input
+                        @input="
+                            () => {
+                                handleClearError('name');
+                            }
+                        "
+                        v-model:value="name"
+                    />
+                    <p v-if="errors.name" class="error-field">Trường này là bắt buộc!</p>
                 </Form.Item>
                 <Form.Item name="password" label="Mật khẩu">
-                    <Input v-model:value="password" />
+                    <Input
+                        @input="
+                            () => {
+                                handleClearError('password');
+                            }
+                        "
+                        v-model:value="password"
+                    />
+                    <p v-if="errors.password" class="error-field">
+                        Mật khẩu tối thiểu 8 ký tự, ít nhất một chữ cái, một số và một ký tự đặc biệt!
+                    </p>
                 </Form.Item>
                 <Form.Item name="email" label="Email">
-                    <Input v-model:value="email" />
+                    <Input
+                        @input="
+                            () => {
+                                handleClearError('email');
+                            }
+                        "
+                        v-model:value="email"
+                    />
+                    <p v-if="errors.email" class="error-field">Trường này phải là email!</p>
                 </Form.Item>
                 <Form.Item name="address" label="Địa chỉ">
-                    <Input v-model:value="address" />
+                    <Input
+                        @input="
+                            () => {
+                                handleClearError('address');
+                            }
+                        "
+                        v-model:value="address"
+                    />
+                    <p v-if="errors.address" class="error-field">Trường này là bắt buộc!</p>
                 </Form.Item>
                 <Form.Item name="gender" label="Giới tính">
-                    <Input v-model:value="gender" />
+                    <Input
+                        @input="
+                            () => {
+                                handleClearError('gender');
+                            }
+                        "
+                        v-model:value="gender"
+                    />
+                    <p v-if="errors.gender" class="error-field">Trường này là bắt buộc!</p>
                 </Form.Item>
                 <Form.Item name="phone_number" label="Số điện thoại">
-                    <InputNumber v-model:value="phone_number" :style="{ width: '100%' }" />
+                    <Input
+                        @input="
+                            () => {
+                                handleClearError('phone_number');
+                            }
+                        "
+                        v-model:value="phone_number"
+                        :style="{ width: '100%' }"
+                    />
+                    <p v-if="errors.phone_number" class="error-field">Trường này phải là số điện thoại!</p>
                 </Form.Item>
                 <Form.Item name="avatar_path" label="Ảnh đại diện (URL)">
-                    <Input v-model:value="avatar_path" />
+                    <Input
+                        @input="
+                            () => {
+                                handleClearError('avatar_path');
+                            }
+                        "
+                        v-model:value="avatar_path"
+                    />
+                    <p v-if="errors.avatar_path" class="error-field">Trường này là bắt buộc!</p>
                 </Form.Item>
                 <Form.Item name="birth_date" label="Ngày sinh">
-                    <Input v-model:value="birth_date" />
+                    <Input
+                        @input="
+                            () => {
+                                handleClearError('birth_date');
+                            }
+                        "
+                        v-model:value="birth_date"
+                    />
+                    <p v-if="errors.birth_date" class="error-field">Trường này là bắt buộc!</p>
                 </Form.Item>
             </Form>
         </div>
     </Modal>
 </template>
+
+<style lang="scss" scoped>
+.error-field {
+    color: red;
+}
+</style>
--- src/components/ModalAddProduct.vue
+++ src/components/ModalAddProduct.vue
@@ -2,7 +2,7 @@
 import { ApiService } from '@/axios/ApiService';
 import { Form, Input, InputNumber, Modal, Select } from 'ant-design-vue';
 import { useToast } from 'primevue/usetoast';
-import { ref, watch } from 'vue';
+import { reactive, ref, watch } from 'vue';
 
 type T_Props = {
     open: any;
@@ -20,8 +20,31 @@ const sub_description = ref<string>('');
 const preview_url = ref<string>('');
 const type = ref<string>('');
 const color = ref<string>('');
-const price = ref<number>();
-const quantity = ref<number>();
+const price = ref<string>('');
+const quantity = ref<string>('');
+
+const errors = reactive<
+    | {
+          name: boolean;
+          description: boolean;
+          sub_description: boolean;
+          preview_url: boolean;
+          type: boolean;
+          color: boolean;
+          price: boolean;
+          quantity: boolean;
+      }
+    | any
+>({
+    name: false,
+    description: false,
+    sub_description: false,
+    preview_url: false,
+    type: false,
+    color: false,
+    price: false,
+    quantity: false,
+});
 
 watch(props.open, () => {
     name.value = '';
@@ -30,54 +53,107 @@ watch(props.open, () => {
     preview_url.value = '';
     type.value = '';
     color.value = '';
-    price.value = 0;
-    quantity.value = 0;
+    price.value = '';
+    quantity.value = '';
 });
 
-const handleFinish = () => {
-    const values = {
-        name: name.value,
-        description: description.value,
-        sub_description: sub_description.value,
-        preview_url: preview_url.value,
-        type: type.value,
-        color: color.value,
-        price: price.value,
-        quantity: quantity.value,
-    };
+const REGEX_NUMBER = /^\d+$/;
 
-    apiService.products
-        .createProduct(values)
-        .then((res) => {
-            if (res.success) {
+const handleError = (name: string) => {
+    errors[`${name}`] = true;
+};
+
+const handleClearError = (name: string) => {
+    errors[`${name}`] = false;
+};
+
+const handleFinish = () => {
+    if (!name.value.trim()) {
+        handleError('name');
+    } else {
+        handleClearError('name');
+    }
+    if (!description.value.trim()) {
+        handleError('description');
+    } else {
+        handleClearError('description');
+    }
+    if (!sub_description.value.trim()) {
+        handleError('sub_description');
+    } else {
+        handleClearError('sub_description');
+    }
+    if (!preview_url.value.trim()) {
+        handleError('preview_url');
+    } else {
+        handleClearError('preview_url');
+    }
+    if (!type.value.trim()) {
+        handleError('type');
+    } else {
+        handleClearError('type');
+    }
+    if (!color.value.trim()) {
+        handleError('color');
+    } else {
+        handleClearError('color');
+    }
+    if (!REGEX_NUMBER.test(price.value.trim())) {
+        handleError('price');
+    } else {
+        handleClearError('price');
+    }
+    if (!REGEX_NUMBER.test(quantity.value.trim())) {
+        handleError('quantity');
+    } else {
+        handleClearError('quantity');
+    }
+
+    if ((Object as any).entries(errors).every((item: any) => item[1] === false)) {
+        const values = {
+            name: name.value,
+            description: description.value,
+            sub_description: sub_description.value,
+            preview_url: preview_url.value,
+            type: type.value,
+            color: +color.value,
+            price: +price.value,
+            quantity: quantity.value,
+        };
+
+        apiService.products
+            .createProduct(values)
+            .then((res) => {
+                if (res.success) {
+                    toast.add({
+                        severity: 'success',
+                        detail: 'Product created successfully',
+                        life: 3000,
+                    });
+                    props.setData({
+                        id: res.data.id,
+                        name: res.data.name,
+                        preview_url: res.data.preview_url,
+                        price: res.data.price,
+                        quantity: res.data.quantity,
+                        rate: res.data.rate,
+                        type: res.data.type,
+                        color: res.data.color,
+                        description: res.data.description,
+                        sub_description: res.data.sub_description,
+                        created_at: res.data.created_at,
+                    });
+                    props.setOpen(false);
+                }
+            })
+            .catch((_) => {
                 toast.add({
-                    severity: 'success',
-                    detail: 'Product created successfully',
+                    severity: 'error',
+                    detail: 'ERROR !!!',
                     life: 3000,
                 });
-                props.setData({
-                    id: res.data.id,
-                    name: res.data.name,
-                    preview_url: res.data.preview_url,
-                    price: res.data.price,
-                    quantity: res.data.quantity,
-                    rate: res.data.rate,
-                    type: res.data.type,
-                    color: res.data.color,
-                    description: res.data.description,
-                    sub_description: res.data.sub_description,
-                    created_at: res.data.created_at,
-                });
-                props.setOpen(false);
-            }
-        })
-        .catch((_) => {
-            toast.add({
-                severity: 'error',
-                detail: 'ERROR !!!',
-                life: 3000,
             });
-        });
+    }
 };
 </script>
 
@@ -94,34 +170,104 @@ const handleFinish = () => {
         <div class="form-container">
             <Form layout="vertical" autoComplete="off" name="update_product" @finish="">
                 <Form.Item name="name" label="Tên sản phẩm">
-                    <Input v-model:value="name" />
+                    <Input
+                        @input="
+                            () => {
+                                handleClearError('name');
+                            }
+                        "
+                        v-model:value="name"
+                    />
+                    <p v-if="errors.name" class="error-field">Trường này là bắt buộc!</p>
                 </Form.Item>
                 <Form.Item name="description" label="Mô tả sản phẩm">
-                    <Input.TextArea v-model:value="description" />
+                    <Input.TextArea
+                        @input="
+                            () => {
+                                handleClearError('description');
+                            }
+                        "
+                        v-model:value="description"
+                    />
+                    <p v-if="errors.description" class="error-field">Trường này là bắt buộc!</p>
                 </Form.Item>
                 <Form.Item name="sub_description" label="Mô tả bổ sung cho sản phẩm">
-                    <Input.TextArea v-model:value="sub_description" />
+                    <Input.TextArea
+                        @input="
+                            () => {
+                                handleClearError('sub_description');
+                            }
+                        "
+                        v-model:value="sub_description"
+                    />
+                    <p v-if="errors.sub_description" class="error-field">Trường này là bắt buộc!</p>
                 </Form.Item>
                 <Form.Item name="preview_url" label="Ảnh sản phẩm">
-                    <Input v-model:value="preview_url" />
+                    <Input
+                        @input="
+                            () => {
+                                handleClearError('preview_url');
+                            }
+                        "
+                        v-model:value="preview_url"
+                    />
+                    <p v-if="errors.preview_url" class="error-field">Trường này là bắt buộc!</p>
                 </Form.Item>
                 <Form.Item name="price" label="Giá sản phẩm">
-                    <InputNumber v-model:value="price" />
+                    <Input
+                        @input="
+                            () => {
+                                handleClearError('price');
+                            }
+                        "
+                        v-model:value="price"
+                    />
+                    <p v-if="errors.price" class="error-field">Trường này phải là 1 số!</p>
                 </Form.Item>
                 <Form.Item name="quantity" label="Số lượng sản phẩm">
-                    <InputNumber v-model:value="quantity" />
+                    <Input
+                        @input="
+                            () => {
+                                handleClearError('quantity');
+                            }
+                        "
+                        v-model:value="quantity"
+                    />
+                    <p v-if="errors.quantity" class="error-field">Trường này phải là 1 số!</p>
                 </Form.Item>
                 <Form.Item name="type" label="Loại sản phẩm">
-                    <Select v-model:value="type">
+                    <Select
+                        @change="
+                            () => {
+                                handleClearError('type');
+                            }
+                        "
+                        v-model:value="type"
+                    >
                         <Select.Option value="dog">Dog</Select.Option>
                         <Select.Option value="cat">Cat</Select.Option>
                         <Select.Option value="food">Food</Select.Option>
                     </Select>
+                    <p v-if="errors.type" class="error-field">Trường này là bắt buộc!</p>
                 </Form.Item>
                 <Form.Item name="color" label="Màu (Thú cưng)">
-                    <Input v-model:value="color" />
+                    <Input
+                        @input="
+                            () => {
+                                handleClearError('color');
+                            }
+                        "
+                        v-model:value="color"
+                    />
+                    <p v-if="errors.color" class="error-field">Trường này là bắt buộc!</p>
                 </Form.Item>
             </Form>
         </div>
     </Modal>
 </template>
+
+<style lang="scss" scoped>
+.error-field {
+    color: red;
+}
+</style>
